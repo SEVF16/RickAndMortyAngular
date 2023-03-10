dc6d99ef985c15a63a3166488d21abaaab8e9b9a
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2023-03-10 13:52:53 -0300

    Avance Angular Rick And Morty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './pages/character/character.component';
+import { DetailCharacterComponent } from './pages/detail-character/detail-character.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
+import { LocationsComponent } from './pages/locations/locations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: CharacterComponent
+  },
+  {
+    path: 'locations',
+    component: LocationsComponent
+  },
+  {
+    path: 'episode',
+    component: EpisodesComponent
+  },
+  {
+    path: 'characters/:id',
+    component: DetailCharacterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MainComponent } from './components/main/main.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { DetailCharacterComponent } from './pages/detail-character/detail-character.component';
+import { LocationsComponent } from './pages/locations/locations.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    CharacterComponent,
+    DetailCharacterComponent,
+    LocationsComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule,
